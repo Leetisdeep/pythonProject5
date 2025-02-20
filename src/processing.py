c4cb50779a5f773222a4779c3ae8ba18b2842a3b
Aleksandrov.E <aaaooogjg@gmail.com>
2025-02-20 11:33:29 +0300

    added docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,8 @@
 def filter_by_state(to_filter: list, state="EXECUTED") -> list:
+    """
+    returns only those elements of the list which state equals to given state
+    if state was not given it will be set to "EXECUTED"
+    """
     only_states = []
     for element in to_filter:
         if state == element["state"]:
@@ -8,5 +12,9 @@ def filter_by_state(to_filter: list, state="EXECUTED") -> list:
 
 
 def sort_by_date(to_sort: list, decreasing=True) -> list:
+    """
+    sorts given list by date
+    decreasing equals True if not given
+    """
     to_sort.sort(key=lambda x: x["date"], reverse=decreasing)
     return to_sort
