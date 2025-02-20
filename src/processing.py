63d539e70bd607f4b111e65bb8a15dac0f47541f
Aleksandrov.E <aaaooogjg@gmail.com>
2025-02-20 11:29:31 +0300

    black , mypy stuff

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,12 @@
-
-def filter_by_state(to_filter:list, state='EXECUTED') -> list:
+def filter_by_state(to_filter: list, state="EXECUTED") -> list:
     only_states = []
-
     for element in to_filter:
-        if state == element['state']:
+        if state == element["state"]:
             only_states.append(element)
 
     return only_states
 
 
-def sort_by_date(to_sort:list, decreasing=True) -> list:
-    to_sort.sort(key=lambda x: x['date'], reverse=decreasing)
+def sort_by_date(to_sort: list, decreasing=True) -> list:
+    to_sort.sort(key=lambda x: x["date"], reverse=decreasing)
     return to_sort
