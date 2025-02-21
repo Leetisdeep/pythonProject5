cf0a333c8208f4a8e5cf01ca63d9398cbf8ea151
Aleksandrov.E <aaaooogjg@gmail.com>
2025-02-21 11:02:49 +0300

    added masks and widget from a project before , added tests module

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция маскировки номера банковской карты"""
+    refactored_card_number: str = str(card_number)
+    return f"{refactored_card_number[:4]} {refactored_card_number[4:6]}** **** {refactored_card_number[-4:]}"
+
+
+def get_mask_account(card_number: int) -> str:
+    """Функция маскировки номера банковского счета"""
+    refactored_card_number: str = str(card_number)
+    return "**" + refactored_card_number[-4:]
---
+++ tests/example.py
@@ -0,0 +1,14 @@
+from src.widget import get_date, mask_account_card
+
+print(get_date('2024-03-11T02:26:18.671407'))
+print(mask_account_card('Visa Platinum 7000792289606361'))
+print(mask_account_card('Счет 73654108430135874305'))
+
+def test_get_date():
+    assert get_date('2024-03-11T02:26:18.671407') == "11.03.2024"
+
+def test_mask_account_card():
+    assert mask_account_card('Visa Platinum 7000792289606361') == "Visa Platinum 7000 79** **** 6361"
+
+test_get_date()
+test_mask_account_card()
